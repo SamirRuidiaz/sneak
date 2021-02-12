779d3ebf944250e6b33c77b964aa034de92137f6
Samir Ruidiaz <samirruidiaz@gmail.com>
2021-02-11 21:27:27 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,16 @@
+const data = [
+  {"name": "CREATIVE LOGO", "type": "Branding", "photo": "1593642532009-6ba71e22f468"},
+  {"name": "CREATIVE LOGO", "type": "Web", "photo": "1612979260294-02b9d8625bce"},
+  {"name": "CREATIVE LOGO", "type": "Photography", "photo": "1612976958534-e01dda4a1a29"},
+  {"name": "CREATIVE LOGO", "type": "Photography", "photo": "1612974002554-8a57ea3c91eb"},
+  {"name": "CREATIVE LOGO", "type": "App", "photo": "1612971974363-75f8b0986b45"},
+  {"name": "CREATIVE LOGO", "type": "Web", "photo": "1613039704361-72da2c68151a"},
+  {"name": "CREATIVE LOGO", "type": "App", "photo": "1612975706873-0822d0f3bebd"},
+  {"name": "CREATIVE LOGO", "type": "Photography", "photo": "1612863144689-f8227e33bff2"},
+  {"name": "CREATIVE LOGO", "type": "Web", "photo": "1613039704541-3b9f2fab1a5b"},
+  {"name": "CREATIVE LOGO", "type": "App", "photo": "1612991236700-86b209b8dfbc"},
+  {"name": "CREATIVE LOGO", "type": "Photography", "photo": "1612991236323-36f92c861cba"},
+  {"name": "CREATIVE LOGO", "type": "Web", "photo": "1593642634367-d91a135587b5"},
+  {"name": "CREATIVE LOGO", "type": "Branding", "photo": "1609252599143-3cad38389d67"},
+  {"name": "CREATIVE LOGO", "type": "App", "photo": "1612991236221-bf8d946f6785"}
+];
